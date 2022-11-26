a6c3a1b69a7bbf4234150891fe3a01af398bdebb
Taiki-nk <taikifate.0206@gmail.com>
2022-11-26 12:18:00 +0900

    :sparkles:[fix] db調整

--- connect_db.php
+++ connect_db.php
@@ -12,9 +12,9 @@ if (! file_exists('../db/urls.sqlite')) {
 try {
     $db = new PDO('sqlite:../db/urls.sqlite');
     // テーブルがなければ作成
-    $sql = 'CREATE TABLE IF NOT EXISTS settings(id INTEGER PRIMARY KEY AUTOINCREMENT,json TEXT,model TEXT,city TEXT,time_stamp TEXT );' ;
+    $sql = 'CREATE TABLE IF NOT EXISTS urls(id INT,json TEXT,time_stamp TEXT );' ;
     $db->query($sql);
-    echo '接続に成功しました。';
 } catch (Exception $e) {
     echo 'DB接続に失敗しました。';
+    exit;
 }
